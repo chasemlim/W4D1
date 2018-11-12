1338f5e326bc46ad4109945f76446fc5bd8ff5e4
chasemlim <chasemlim@gmail.com>
2018-11-12 15:08:14 -0800

    Implement UsersController#show, #update, #destroy

--- first_routes/app/controllers/users_controller.rb
+++ first_routes/app/controllers/users_controller.rb
@@ -5,10 +5,44 @@ class UsersController < ApplicationController
   end
 
   def create
-    render json: params
+    user = User.new(user_params)
+    if user.save
+      render json: user
+    else
+      render json: user.errors.full_messages, status: :unprocessable_entity
+    end
   end
 
   def show
-    render json: params
+    user = User.find_by(id: params[:id])
+    if user
+      render json: user
+    else
+      redirect_to action: "index"
+    end
+  end
+
+  def update
+    user = User.find_by(id: params[:id])
+
+    if user
+      user.update(user_params)
+    else
+      render plain: "There was an error updating user with id #{params[:id]}"
+    end
+  end
+
+  def destroy
+    user = User.find_by(id: params[:id])
+
+    if user
+      user.destroy
+    else
+      render plain: "There was an error destroying user with id #{params[:id]}"
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email)
   end
 end
